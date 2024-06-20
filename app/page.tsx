30493f8e1ad2feac2d33e42001731737a21e311c
Newspring08 <solomonemma013@gmail.com>
2024-06-20 11:45:48 +0100

    git init for nextjs dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-30">
         { <AcmeLogo /> }
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -28,11 +28,11 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <div className="flex items-center justify-center p-6 md:w-2/5 md:px-28 md:py-12">
           { }
           <Image 
             src="/hero-desktop.png"
-            width={1000}
+            width={900}
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
